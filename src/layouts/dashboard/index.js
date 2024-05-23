2b027f3eee74b3bd12f56bea645d1949e87c7909
hafsakzoun <118023551+hafsakzoun@users.noreply.github.com>
2024-05-23 18:50:06 +0100

    charts

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -8,7 +8,6 @@ import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
 import MiniStatisticsCard from "examples/Cards/StatisticsCards/MiniStatisticsCard";
-import colors from "assets/theme/base/colors";
 import WelcomeMark from "layouts/dashboard/components/WelcomeMark";
 import Projects from "layouts/dashboard/components/Projects";
 import OrderOverview from "layouts/dashboard/components/OrderOverview";
@@ -23,20 +22,27 @@ import { FaShoppingCart } from "react-icons/fa";
 function Dashboard() {
   const [counts, setCounts] = useState({ prediction_0: 0, prediction_1: 0, total_clients: 0 });
   const [chartData, setChartData] = useState([]);
-
+  const [chartData2, setChartData2] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const predictionsResponse = await axios.get('http://localhost:5000/count_predictions');
         const totalClientsResponse = await axios.get('http://localhost:5000/total_clients');
         const response = await axios.get("http://localhost:5000/churn_data");
+        const response2 = await axios.get("http://localhost:5000/churn_data2");
         // Process the data to transform it into the required format
         const formattedData = response.data.map((item, index) => ({
           x: index + 1,
           y: item.probability[1] // Assuming you want the second probability
-        }));        
+        })); 
+        
+        const formattedData2 = response2.data.map((item, index) => ({
+          x: index + 1,
+          y: item.probability[1] // Assuming you want the second probability
+        }));
         
         setChartData(formattedData);
+        setChartData2(formattedData2);
         setCounts({
           prediction_0: predictionsResponse.data.prediction_0,
           prediction_1: predictionsResponse.data.prediction_1,
@@ -103,11 +109,11 @@ function Dashboard() {
 
         <VuiBox mb={3} >
           <Grid container spacing={3}>
-            <Grid item xs={12} lg={6} xl={7}>
+            <Grid item xs={12} lg={6} xl={12}>
               <Card>
                 <VuiBox sx={{ height: "100%" }}>
                   <VuiTypography variant="lg" color="white" fontWeight="bold" mb="5px">
-                    Churn Prediction Probability
+                    Churn Prediction 1 Probability
                   </VuiTypography>
                   <VuiBox display="flex" alignItems="center" mb="40px"></VuiBox>
                   <VuiBox sx={{ height: "310px" }}>
@@ -116,10 +122,21 @@ function Dashboard() {
                 </VuiBox>
               </Card>
             </Grid>
+            <Grid item xs={12} lg={6} xl={12}>
+              <Card>
+                <VuiBox sx={{ height: "100%" }}>
+                  <VuiTypography variant="lg" color="white" fontWeight="bold" mb="5px">
+                    Churn Prediction 0 Probability
+                  </VuiTypography>
+                  <VuiBox display="flex" alignItems="center" mb="40px"></VuiBox>
+                  <VuiBox sx={{ height: "310px" }}>
+                    <LineChart lineChartData={chartData2} />
+                  </VuiBox>
+                </VuiBox>
+              </Card>
+            </Grid>
           </Grid>
         </VuiBox>
-        
-
         <Grid container spacing={3} direction="row" justifyContent="center" alignItems="stretch">
           <Grid item xs={12} md={6} lg={8}>
             <Projects />
